0ecba9e71d5ee0448c924d27229998b857e5c2f9
juanapu <974843350@qq.com>
2018-04-17 22:39:15 +0800

    20180417

---
+++ src/container/postlist.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Postlist extends Component{
+	render(){
+		return(
+				<div>here is Index</div>
+			)
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import { Component} from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise'; 
 
-import App from './components/app';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import Postlist from './container/postlist';
+import Createpost from './container/createpost';
+
+
 import Reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(Reducers)}>
-    <App />
+	     <BrowserRouter>
+	    	<div>
+	    	  <Switch>
+  	    	    <Route path='/createpost/' component={Createpost} />
+	    	    <Route path='/' component={Postlist} />
+	    	  </Switch>
+  	      </div>
+	    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
